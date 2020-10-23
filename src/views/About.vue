dd50f425c4d4004522d292142ca7a8e3e6d07242
LuckyWind_sck <luckywindsck@gmail.com>
2020-10-23 20:47:22 +0900

    feat: add links in About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <span
+      v-for="([name, link], index) in Object.entries(this.sites)"
+      :key="index"
+    >
+      <span v-if="index > 0"> | </span>
+      <a target="_blank" rel="noopener noreferrer" :href="link">{{ name }}</a>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'About',
+  data() {
+    return {
+      sites: {
+        repo: 'https://github.com/LuckyWindsck/sck-oekaki',
+        github: 'https://github.com/LuckyWindsck',
+        twitter: 'https://twitter.com/LuckyWind_sck',
+      },
+    };
+  },
 };
 </script>
 
