b396366f082dee0a5299adffe9fb09cf4915b9dc
LuckyWind_sck <luckywindsck@gmail.com>
2020-10-23 15:22:53 +0900

    feat: setup p5.js environment

--- src/views/Sketch.vue
+++ src/views/Sketch.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>Sketch</div>
+  <div id="sketch"></div>
 </template>
 
 <script>
+import P5 from 'p5';
+
+const sketch = (p5) => {
+  p5.setup = () => {
+    p5.createCanvas(400, 400);
+  };
+
+  p5.draw = () => {
+    p5.background(220);
+  };
+};
+
 export default {
   name: 'Sketch',
+  mounted() {
+    this.p5 = new P5(sketch, 'sketch');
+  },
 };
 </script>
 
