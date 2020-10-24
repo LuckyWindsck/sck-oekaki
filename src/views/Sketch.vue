61bae1a424ff8016152d0f14a73c49a1fe55cc77
LuckyWind_sck <luckywindsck@gmail.com>
2020-10-25 01:12:12 +0900

    feat: stop random line rendering & draw random point in circle

--- src/views/Sketch.vue
+++ src/views/Sketch.vue
@@ -5,7 +5,6 @@
 <script>
 import P5 from 'p5';
 import Point from '../util/p5/shape/2d-primitives/point';
-import Line from '../util/p5/shape/2d-primitives/line';
 import Circle from '../util/p5/shape/2d-primitives/circle';
 import '../util/p5/rendering/extend';
 
@@ -16,7 +15,7 @@ const sketch = (p5) => {
     const x = randomSign() * p5.random(0, circle.radius);
     const y = randomSign() * p5.random(0, Math.sqrt(circle.radius ** 2 - x ** 2));
 
-    return new Point(x + circle.center.x, y + circle.center.y);
+    return new Point(x + circle.center.x, y + circle.center.y, p5);
   };
 
   const canvasSize = 800;
@@ -24,17 +23,17 @@ const sketch = (p5) => {
   const radius = canvasSize * 0.4;
   const circle = new Circle(center, radius, p5);
 
+  const startPoint = createPointInCircle(circle);
+
   p5.setup = () => {
     p5.createSquareCanvas(canvasSize);
-    p5.background('white');
 
+    p5.background('white');
     circle.show();
   };
 
   p5.draw = () => {
-    const line = new Line(...Array(2).fill(circle).map(createPointInCircle), p5);
-
-    line.show();
+    startPoint.show({ strokeWeight: 10 });
   };
 };
 
