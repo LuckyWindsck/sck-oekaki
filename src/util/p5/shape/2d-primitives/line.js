644db90dc6452cb23684b99c3a9ed60a400d0fb0
LuckyWind_sck <luckywindsck@gmail.com>
2020-10-25 02:50:26 +0900

    feat: calculate yIntercept / orthogonalSlope / polar-form of a line

--- src/util/p5/shape/2d-primitives/line.js
+++ src/util/p5/shape/2d-primitives/line.js
@@ -7,8 +7,34 @@ class Line extends TwoDPrimitive {
 
     this.point1 = point1;
     this.point2 = point2;
+  }
 
-    this.slope = (this.point2.y - this.point1.y) / (this.point2.x - this.point1.x);
+  get deltaX() {
+    return this.point2.x - this.point1.x;
+  }
+
+  get deltaY() {
+    return this.point2.y - this.point1.y;
+  }
+
+  get slope() {
+    return this.deltaY / this.deltaX;
+  }
+
+  get yIntercept() {
+    return this.point1.y - this.slope * this.point1.x;
+  }
+
+  get radius() {
+    return Math.hypot(this.deltaX, this.deltaY);
+  }
+
+  get theta() {
+    return Math.atan(this.slope) + Number(this.deltaX < 0) * Math.PI;
+  }
+
+  get orthogonalSlope() {
+    return -1 / this.slope;
   }
 
   show(option) {
